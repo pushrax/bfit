57145b0e9fa0d335749179e142d8f6719acc8ad7
Justin Li <jli.justinli@gmail.com>
2012-09-28 13:48:16 -0400

    Optimize code size slightly

--- bfit.c
+++ bfit.c
@@ -33,7 +33,7 @@ BfitInsn *bfit_lex(const char *source, unsigned int *total_out)
     c = source[i];
     if (c == '>' || c == '<' || c == '+' || c == '-')
     {
-      for (j = i + 1; j < length; ++j) if (c != source[j]) break;
+      for (j = i + 1; j < length && j - i < 255; ++j) if (c != source[j]) break;
       i = j - 1;
       ++total;
     }
@@ -51,7 +51,7 @@ BfitInsn *bfit_lex(const char *source, unsigned int *total_out)
     c = source[i];
     if (c == '>' || c == '<' || c == '+' || c == '-')
     {
-      for (j = i + 1; j < length; ++j) if (c != source[j]) break;
+      for (j = i + 1; j < length && j - i < 255; ++j) if (c != source[j]) break;
       insns[index++] = (BfitInsn) { c, j - i };
       i = j - 1;
     }
@@ -71,23 +71,23 @@ BfitInsn *bfit_lex(const char *source, unsigned int *total_out)
  */
 byte *bfit_compile(const BfitInsn *insns, unsigned int count, const byte *data, const char *input, unsigned int *length)
 {
-  unsigned int cl = 0, i, dataptr = (unsigned int) data, offset;
+  unsigned int cl = 0, i, offset;
   byte *code;
 
   // Pre-calculate length of machine code
   for (i = 0; i < count; ++i)
   {
     char type = insns[i].type;
-    if (type == '>' || type == '<') cl += 5;
-    else if (type == '+' || type == '-') cl += 7;
-    else if (type == '[') cl += 13;
+    if (type == '>' || type == '<') cl += 3;
+    else if (type == '+' || type == '-') cl += 3;
+    else if (type == '[') cl += 9;
     else if (type == ']') cl += 5;
-    else if (type == '.') cl += 26;
-    else if (type == ',') cl += 18;
+    else if (type == '.') cl += 21;
+    else if (type == ',') cl += 14;
   }
 
-  cl += 9; // init
-  cl += 10; // de-init
+  cl += 13; // init
+  cl += 6; // de-init
 
   code = malloc(cl);
   cl = 0;
@@ -103,11 +103,12 @@ byte *bfit_compile(const BfitInsn *insns, unsigned int count, const byte *data, 
   code[cl++] = 0xe5;
 
   // Set up data pointer
-  code[cl++] = 0xb8; // mov eax, 0
-  *(unsigned int *)(code + cl) = 0; cl += 4;
+  code[cl++] = 0xb8; // mov eax, data
+  *(unsigned int *)(code + cl) = (unsigned int) data; cl += 4;
 
   // Set up input pointer
-  code[cl++] = 0x50; // push eax
+  code[cl++] = 0x68; // push 0
+  *(unsigned int *)(code + cl) = 0; cl += 4;
 
   // Generate code
   for (i = 0; i < count; ++i)
@@ -115,30 +116,29 @@ byte *bfit_compile(const BfitInsn *insns, unsigned int count, const byte *data, 
     switch (insns[i].type)
     {
       case '>':
-        code[cl++] = 0x05; // add eax, times
-        *(unsigned int *)(code + cl) = insns[i].times; cl += 4;
+        code[cl++] = 0x83; // add eax, byte times
+        code[cl++] = 0xc0;
+        code[cl++] = insns[i].times;
         break;
       case '<':
-        code[cl++] = 0x2d; // sub eax, times
-        *(unsigned int *)(code + cl) = insns[i].times; cl += 4;
+        code[cl++] = 0x83; // sub eax, byte times
+        code[cl++] = 0xe8;
+        code[cl++] = insns[i].times;
         break;
       case '+':
-        code[cl++] = 0x80; // add byte [data+eax], times
-        code[cl++] = 0x80;
-        *(unsigned int *)(code + cl) = dataptr; cl += 4;
+        code[cl++] = 0x80; // add byte [eax], times
+        code[cl++] = 0x00;
         code[cl++] = insns[i].times;
         break;
       case '-':
-        code[cl++] = 0x80; // sub byte [data+eax], times
-        code[cl++] = 0xa8;
-        *(unsigned int *)(code + cl) = dataptr; cl += 4;
+        code[cl++] = 0x80; // sub byte [eax], times
+        code[cl++] = 0x28;
         code[cl++] = insns[i].times;
         break;
       case '[':
         stack[stackptr++] = cl;
-        code[cl++] = 0x80; // cmp byte [data+eax], 0
-        code[cl++] = 0xb8;
-        *(unsigned int *)(code + cl) = dataptr; cl += 4;
+        code[cl++] = 0x80; // cmp byte [eax], 0
+        code[cl++] = 0x38;
         code[cl++] = 0x00;
         code[cl++] = 0x0f; // jz/je offset
         code[cl++] = 0x84;
@@ -150,15 +150,13 @@ byte *bfit_compile(const BfitInsn *insns, unsigned int count, const byte *data, 
         *(int *)(code + cl) = -offset; cl += 4;
         
         // fill in offset for matching '['
-        *(unsigned int *)(code + stack[stackptr] + 9) = cl - stack[stackptr] - 13;
+        *(unsigned int *)(code + stack[stackptr] + 5) = cl - stack[stackptr] - 9;
         break;
       case '.':
         code[cl++] = 0x50; // push eax
         code[cl++] = 0xba; // mov edx, 1
         *(unsigned int *)(code + cl) = 1; cl += 4;
-        code[cl++] = 0xb9; // mov ecx, data
-        *(unsigned int *)(code + cl) = dataptr; cl += 4;
-        code[cl++] = 0x01; // add ecx, eax
+        code[cl++] = 0x89; // mov ecx, eax
         code[cl++] = 0xc1;
         code[cl++] = 0xbb; // mov ebx, 1
         *(unsigned int *)(code + cl) = 1; cl += 4;
@@ -175,9 +173,8 @@ byte *bfit_compile(const BfitInsn *insns, unsigned int count, const byte *data, 
         code[cl++] = 0x8a; // mov bl, [input+ebx]
         code[cl++] = 0x9b;
         *(unsigned int *)(code + cl) = (unsigned int) input; cl += 4;
-        code[cl++] = 0x88; // mov byte [data+eax], bl
-        code[cl++] = 0x98;
-        *(unsigned int *)(code + cl) = dataptr; cl += 4;
+        code[cl++] = 0x88; // mov byte [eax], bl
+        code[cl++] = 0x18;
         code[cl++] = 0xff; // inc dword [esp]
         code[cl++] = 0x04;
         code[cl++] = 0x24;
@@ -189,10 +186,9 @@ byte *bfit_compile(const BfitInsn *insns, unsigned int count, const byte *data, 
   code[cl++] = 0x5b; // pop ebx
 
   // Unwind stack and return [data+eax]
-  code[cl++] = 0x0f; // movzx eax, byte [data+eax]
+  code[cl++] = 0x0f; // movzx eax, byte [eax]
   code[cl++] = 0xb6;
-  code[cl++] = 0x80;
-  *(unsigned int *)(code + cl) = dataptr; cl += 4;
+  code[cl++] = 0x00;
   code[cl++] = 0xc9; // leave
   code[cl++] = 0xc3; // ret
 
